0dfddd6d27cc35a0a47ffee57c12947155840865
alllexxx <freeallex@gmail.com>
2018-09-26 00:53:11 +0300

    Add load template method

--- SolutionEquations/App_Code/EquationHandler.cs
+++ SolutionEquations/App_Code/EquationHandler.cs
@@ -45,12 +45,8 @@ namespace SolutionEquations.App_Code
                     }
                     solution = SolutionEquation.Resolve(parametersEquation[0], parametersEquation[1], parametersEquation[2]);
                 }
-
-                string pathTemplate = String.Concat(request.MapPath("/"),  "template/"));
-                StreamReader sr = new StreamReader(String.Concat(pathTemplate, "layout"));
-
-                string template = sr.ReadToEnd();
-                response.Write(String.Format(template, solution));
+                
+                response.Write(String.Format(LoadTemplate(request, "template", "layout"), solution));
             }
             catch (Exception e)
             {
@@ -58,7 +54,20 @@ namespace SolutionEquations.App_Code
             }
         }
 
-       
+        private string LoadTemplate(HttpRequest request, string dirTemplate, string template)
+        {
+            try
+            {
+                string pathTemplate = String.Concat(request.MapPath("/"), String.Concat(dirTemplate, "/"));
+                StreamReader sr = new StreamReader(String.Concat(pathTemplate, template));
+                
+                return sr.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error:" + e);
+            }
+        }
         public bool IsReusable => false;
     }
 }
