048c97ab0450144651f36c117a58c1f5010c3faf
chmakalexandr <chmak.av@gmail.com>
2018-10-03 16:53:50 +0300

    Add processing POST method

--- SolutionEquations/App_Code/EquationHandler.cs
+++ SolutionEquations/App_Code/EquationHandler.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Web;
 
 namespace SolutionEquations.App_Code
@@ -18,22 +15,40 @@ namespace SolutionEquations.App_Code
 
             try
             {
-                string strParams = request.FilePath;
+                string solution = "Solution was not found. Check the parameters quadratic equation.";
+                string equation = "";
 
-                if (strParams.Length>1 & strParams[strParams.Length-1] == '/')
+                string[] strParameters = new string[3];
+                double[] parametersEquation = { 0, 0, 0 };
+
+                if (request.HttpMethod == "POST")
                 {
-                    strParams = strParams.Remove(strParams.Length-1);
+                    var form = request.Form;
+                    if (form.HasKeys())
+                    {
+                        string[] arrKey = form.AllKeys;
+                        for (int i = 0; i < arrKey.Length; i++)
+                        {
+                            strParameters[i] = form.Get(i); ;                                   
+                        }
+                    }
                 }
+                else if (request.HttpMethod == "GET")
+                {
+                    StringBuilder strParams = new StringBuilder(request.FilePath);
 
-                strParams = strParams.Remove(0, 1);   //delete first '/'
+                    if (strParams.Length > 1 & strParams[strParams.Length - 1] == '/')
+                    {
+                        strParams.Remove(strParams.Length - 1, 1); //delete in the end '/'
+                    }
+
+                    strParams = strParams.Remove(0, 1); //delete beginning '/'
+                    
+                    strParameters = strParams.ToString().Split('/');
+                }
                 
-                string solution = "Solution was not found. Check the parameters.";
-               
-                double[] parametersEquation = {0, 0, 0};
-                    
-                string[] strParameters = strParams.Split('/');
-                    
-                if (SolutionEquation.CheckIsDoubleParameters(strParameters) & strParameters.Length<=3)
+
+                if (SolutionEquation.StrIsDouble(strParameters) & strParameters.Length<=3)
                 {
                     int i = 0;
                     foreach (string parameter in strParameters)
@@ -43,10 +58,16 @@ namespace SolutionEquations.App_Code
                         parametersEquation[i] = Convert.ToDouble(sb.ToString());
                         i++;
                     }
-                    solution = SolutionEquation.Resolve(parametersEquation[0], parametersEquation[1], parametersEquation[2]);
+
+                    equation = SolutionEquation.FormatEquation(parametersEquation[0], parametersEquation[1], parametersEquation[2]);
+
+                    if (SolutionEquation.Resolve(parametersEquation[0], parametersEquation[1], parametersEquation[2])[0] != null)
+                    {
+                        solution = String.Format("First root is {0}.</br> Second root is {1}.", SolutionEquation.Resolve(parametersEquation[0], parametersEquation[1], parametersEquation[2]));
+                    }
                 }
                 
-                response.Write(String.Format(LoadTemplate(request, "template", "layout"), solution));
+                response.Write(String.Format(LoadTemplate(request, "template", "layout"), equation, solution));
             }
             catch (Exception e)
             {
@@ -58,16 +79,21 @@ namespace SolutionEquations.App_Code
         {
             try
             {
-                string pathTemplate = String.Concat(request.MapPath("/"), String.Concat(dirTemplate, "/"));
-                StreamReader sr = new StreamReader(String.Concat(pathTemplate, template));
+                StringBuilder pathTemplate = new StringBuilder(request.MapPath("/"));
+                pathTemplate.Append(dirTemplate);
+                pathTemplate.Append("/");
+                pathTemplate.Append(template);
+
+                StreamReader sr = new StreamReader(pathTemplate.ToString());
                 
                 return sr.ReadToEnd();
             }
             catch (Exception e)
             {
-                throw new Exception("Error:" + e);
+                throw new Exception("Template error:" + e);
             }
         }
+
         public bool IsReusable => false;
     }
 }
